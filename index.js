e683e690b7f1487c01f302bd09ad861f460dd904
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-08-30 22:46:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+// function multiply() {
+//   const result = num1 * num2;
+//   return result;
+// }
+
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10 ) + 1;
+
+const num3 = 16;
+const num4 = 4;
+
+const mod  = num3 / num4;
+
+const max = Math.max (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
